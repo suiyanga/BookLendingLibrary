a9d0bc6a43955cee9da9fd6ec5fbf7ed58d00c61
Suiyanga. <138379275+suiyanga@users.noreply.github.com>
2025-02-16 20:05:02 +0300

    CI pipeline update

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -5,14 +5,17 @@ FactoryBot.define do
       sequence(:isbn) { |n| "978-#{n.to_s.rjust(10, '0')}" }
       description { Faker::Lorem.paragraph }
       image_url { Rails.env.test? ? "https://via.placeholder.com/150" : "https://your-production-url.com/your-image.jpg" }
-
-
+  
       after(:build) do |book|
         # Ensure the ISBN is unique across all books in the database
         loop do
           random_isbn = "978-#{rand(1000000000..9999999999)}"
           break if Book.where(isbn: random_isbn).empty?
+  
+          # Assign the unique ISBN to the book
+          book.isbn = random_isbn
         end
+      end
     end
   end
-end
+  

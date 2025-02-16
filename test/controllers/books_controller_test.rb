6a3c8ff5a51cc86f549796aa3548689b957289fe
Suiyanga. <138379275+suiyanga@users.noreply.github.com>
2025-02-16 20:52:59 +0300

    CI pipeline update

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -2,14 +2,28 @@ require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
   include Rails.application.routes.url_helpers
+
+  # Test for index action
   test "should get index" do
-    get books_index_url
+    # Use FactoryBot to create a book
+    book = FactoryBot.create(:book)
+
+    # Make the request to the index action
+    get books_url
+
+    # Assert that the response is successful
     assert_response :success
   end
 
+  # Test for show action
   test "should get show" do
-    book = books(:one) 
-    get books_show_url
+    # Use FactoryBot to create a book
+    book = FactoryBot.create(:book)
+
+    # Make the request to the show action with the created book's id
+    get books_url(book)
+
+    # Assert that the response is successful
     assert_response :success
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,9 +2,11 @@ require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   include Rails.application.routes.url_helpers
+
   test "should get show" do
-    user = users(:one)
-    get users_show_url(user)
+    user = FactoryBot.create(:user)  # Use FactoryBot to create a user
+    sign_in user  # Devise method to sign in the user
+    get user_url(user)
     assert_response :success
-end
+  end
 end

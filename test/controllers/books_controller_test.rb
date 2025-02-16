8522e5d86d8861de8d39728d4497e4fe996da604
Suiyanga. <138379275+suiyanga@users.noreply.github.com>
2025-02-16 20:46:30 +0300

    CI pipeline update

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,12 +1,14 @@
 require "test_helper"
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
+  include Rails.application.routes.url_helpers
   test "should get index" do
     get books_index_url
     assert_response :success
   end
 
   test "should get show" do
+    book = books(:one) 
     get books_show_url
     assert_response :success
   end
